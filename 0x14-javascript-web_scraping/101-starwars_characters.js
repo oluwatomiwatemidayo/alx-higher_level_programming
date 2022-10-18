4f19fe41ebdeec2500d31fdb346fae9e076d7bb1
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-10-18 19:22:10 +0100

    All files modified

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const url = 'https://swapi.co/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (!error) {
-    const characters = JSON.parse(body).characters;
+    let characters = JSON.parse(body).characters;
     printCharacters(characters, 0);
   }
 });
